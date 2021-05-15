7894bf97f32776dabe83c1e53be61069bc7092e0
Li Yang <1175529741@qq.com>
2021-05-16 00:58:31 +0800

    lc0011

---
+++ include/lc0011_container_with_most_water.h
@@ -0,0 +1,12 @@
+/*
+ * LeetCode算法练习
+ * Description  : LeetCode题库11题，https://leetcode-cn.com/problems/container-with-most-water/
+ * Author       : 李阳
+ * Created      : 2021-05-16
+ */
+#ifndef LC0011_CONTAINER_WITH_MOST_WATER_H
+#define LC0011_CONTAINER_WITH_MOST_WATER_H
+
+void LC0011Test(void);
+
+#endif // LC0011_CONTAINER_WITH_MOST_WATER_H
