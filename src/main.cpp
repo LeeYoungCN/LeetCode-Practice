ad52aea9321567a3654a14e8f845a7d76dfefd38
Li Yang <1175529741@qq.com>
2021-05-26 22:43:02 +0800

    LC0621

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include "lc0011_container_with_most_water.h"
-#include "lc0406_queue_reconstruction_by_height.h"
+#include "lc0621_task_scheduler.h"
 
 int main(int argc, char *argv[]) {
-    LC0406Test();
+    LC0621Test();
     return 0;
 }
