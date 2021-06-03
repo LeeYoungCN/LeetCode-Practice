4cb6f9836818e70176a1a3d7984640d321fc4019
Li Yang <42291832+LeeYoungCN@users.noreply.github.com>
2021-06-03 14:19:15 +0800

    Update leetcode_head.h

--- LeetCode/include/leetcode_head.h
+++ LeetCode/include/leetcode_head.h
@@ -22,6 +22,7 @@
 #include "lc0094_binary_tree_inorder_traversal.h"
 #include "lc0406_queue_reconstruction_by_height.h"
 #include "lc0523_continuous_subarray_sum.h"
+#include "lc0525_contiguous_array.h"
 #include "lc0621_task_scheduler.h"
 #include "lc0994_rotting_oranges.h"
 #include "lc1744_can_you_eat_your_favorite_candy_on_your_favorite_day.h"
