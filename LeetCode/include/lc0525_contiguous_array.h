208eb2d7f81605c337812666902766606b23ecbb
Li Yang <42291832+LeeYoungCN@users.noreply.github.com>
2021-06-03 14:16:59 +0800

    add lc0525头文件

---
+++ LeetCode/include/lc0525_contiguous_array.h
@@ -0,0 +1,33 @@
+/*
+ * LeetCode算法练习
+ * Description  : LeetCode题库525题, https://leetcode-cn.com/problems/contiguous-array/description/
+ * Author       : 李阳
+ * Created      : 2021-06-03
+ * 
+ * [525]连续数组
+ *
+ *
+ *https://leetcode-cn.com/problems/contiguous-array/description/
+ *
+ *
+ * 给定一个二进制数组, 找到含有相同数量的 0 和 1 的最长连续子数组（的长度）。
+ *  
+ * 示例 1:
+ * 输入: [0,1]
+ * 输出: 2
+ * 说明: [0, 1] 是具有相同数量0和1的最长连续子数组。
+ * 示例 2:
+ * 输入: [0,1,0]
+ * 输出: 2
+ * 说明: [0, 1] (或 [1, 0]) 是具有相同数量0和1的最长连续子数组。
+ *  
+ * 注意: 给定的二进制数组的长度不会超过50000。
+ * 
+ */
+
+#ifndef LC0525_CONTIGUOUS_ARRAY_H
+#define LC0525_CONTIGUOUS_ARRAY_H
+
+void LC0525Test(void);
+
+#endif // LC0525_CONTIGUOUS_ARRAY_H
