214ebeba02f24e05055534cdb6d7ae72f0a008da
Li Yang <42291832+LeeYoungCN@users.noreply.github.com>
2021-06-18 21:52:35 +0800

    Add files via upload

---
+++ LeetCode/include/lc0279_perfect_squares.h
@@ -0,0 +1,29 @@
+/*
+ * LeetCode算法练习
+ * Description  : LeetCode题库279题, https://leetcode-cn.com/problems/perfect-squares/description/
+ * Author       : 李阳
+ * Created      : 2021-06-05
+ *
+ * [279]完全平方数
+ *
+ *
+ *https://leetcode-cn.com/problems/perfect-squares/description/
+ *
+ *
+ * 给定正整数 n，找到若干个完全平方数（比如 1, 4, 9, 16, ...）使得它们的和等于 n。你需要让组成和的完全平方数的个数最少。
+ * 示例 1:
+ * 输入: n = 12
+ * 输出: 3 
+ * 解释: 12 = 4 + 4 + 4.
+ * 示例 2:
+ * 输入: n = 13
+ * 输出: 2
+ * 解释: 13 = 4 + 9.
+ * 
+ */
+#ifndef LC0279_PERFECT_SQUARES_H
+#define LC0279_PERFECT_SQUARES_H
+
+void LC0279Test();
+
+#endif // LC0279_PERFECT_SQUARES_H
