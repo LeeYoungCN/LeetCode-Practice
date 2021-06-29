f8902d2bcbdddafdfa563f4c5455d1be8d1c1f7c
Li Yang <1175529741@qq.com>
2021-06-29 23:14:25 +0800

    lc0149_max_points_on_a_line

--- LeetCode/src/lc0149_max_points_on_a_line.cpp
+++ LeetCode/src/lc0149_max_points_on_a_line.cpp
@@ -17,28 +17,44 @@ class Solution {
 public:
     int maxPoints(vector<vector<int>>& points) {
         const int N = points.size();
+        const int maxY = 20000;
         int maxNum = 1;
-        if (N == 1) {
-            return 1;
+        if (N <= 2) {
+            return N;
         }
 
         for (int i = 0; i < N - 1; i++) {
-            vector<int> point_i = points[i];
-            unordered_map<float, int> gradMap;
+            unordered_map<int, int> gradMap;
             for (int j = i + 1; j < N; j++) {
-                vector<int> point_j = points[j];
-                int deltaX = point_j[0] - point_i[0];
-                int deltaY = point_j[1] - point_i[1];
-                float gradient = (deltaX ? (float)deltaY/deltaX: INT_MAX);
+                int deltaX = points[j][0] - points[i][0];
+                int deltaY = points[j][1] - points[i][1];
+                if (deltaX == 0) {
+                    deltaY = 1;
+                } else if (deltaY == 0) {
+                    deltaX = 1;
+                }
+                if (deltaY < 0) {
+                    deltaX = -deltaX;
+                    deltaY = -deltaY;
+                }
+                int gcdXY = gcd(deltaX, deltaY);
+                deltaX = deltaX/gcdXY;
+                deltaY = deltaY/gcdXY;
+
+                int gradient = deltaY + deltaX * (maxY + 1);
                 gradMap[gradient] ++;
             }
-
             for (auto p: gradMap) {
                 maxNum = max(p.second, maxNum);
-                cout << maxNum << endl;
             }
         }
         return maxNum + 1;
+    }
+
+private:
+    int gcd(int a, int b)
+    {
+        return b? gcd(b, a%b): a;
     }
 };
 
