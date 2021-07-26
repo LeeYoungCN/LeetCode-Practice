dff6f4f2258ca73a2bef06bf6bbf0fc2a55fe7aa
徐营营 <xuyingying@xuyingyingdeMacBook-Pro.local>
2021-07-26 22:06:38 +0800

    iterator

--- main.cpp
+++ main.cpp
@@ -2,9 +2,53 @@
 #include <algorithm>
 #include <vector>
 
+#include <iterator>
+
 #include "leetcode_head.h"
 using namespace std;
-int main(int argc, char *argv[]) {
-    LC1743Test();
+
+class MyIterator:iterator<input_iterator_tag, int> {
+    int* p = NULL;
+public:
+    MyIterator(int* x):p(x) {}
+    MyIterator(const MyIterator& mit):p(mit.p) {}
+    MyIterator& operator++ ()
+    {
+        this->p++;
+        cout << "operator++" << endl;
+        return *this;
+    }
+    MyIterator operator++ (int) 
+    {
+        MyIterator tmp(*this);
+        operator++();
+        cout << "operator++ int" << endl;
+        return *this;
+    }
+    int& operator*()
+    {
+        return *p;
+    }
+    bool operator== (MyIterator other)
+    {
+        return p==other.p;
+    }
+    bool operator!= (MyIterator other)
+    {
+        return p!=other.p;
+    }
+};
+
+int main(int argc, char *argv[]) 
+{
+    int list[] = {0,1,2,3,4,5,6};
+    MyIterator begin(list);
+    MyIterator end(list+7);
+    for (MyIterator it = begin; it != end; it++) {
+        cout << *it << endl;
+    }
+    
+
     return 0;
 }
+
