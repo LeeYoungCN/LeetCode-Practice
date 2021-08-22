9021981ff5b69017b87ba45cc53e792f705a9b24
徐营营 <xuyingying@xuyingyingdeMacBook-Pro.local>
2021-08-22 20:21:50 +0800

    lc0516

--- LeetCode/src/lc0516_longest_palindromic_subsequence.cpp
+++ LeetCode/src/lc0516_longest_palindromic_subsequence.cpp
@@ -15,17 +15,33 @@
 #include <unordered_set>
 #include <unordered_map>
 
+using namespace std;
+
 #include "public_function.h"
 #include "lc0516_longest_palindromic_subsequence.h"
 
 class Solution {
 public:
     int longestPalindromeSubseq(string s) {
-        return 0;
+        const int N = s.length();
+        vector<vector<int>> dp(N, vector<int>(N));
+        for (int i = N - 1; i >= 0; i--) {
+            dp[i][i] = 1;
+            for (int j = i + 1; j < N; j++) {
+                if (s[i] == s[j]) {
+                    dp[i][j] = dp[i + 1][j - 1] + 2;
+                } else {
+                    dp[i][j] = max(dp[i+1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return dp[0][N - 1];
     }
 };
 
 void LC0516Test()
 {
-    ;
+    string s = "abxa";
+    Solution solu;
+    cout << solu.longestPalindromeSubseq(s) << endl;
 }
