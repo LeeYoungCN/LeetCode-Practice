9bd8a4124d4e0cdc27b1fec82a25b35115d8b69c
徐营营 <xuyingying@xuyingyingdeMacBook-Pro.local>
2021-10-06 14:38:22 +0800

    libCodeFile

--- Tools/build_tool.sh
+++ Tools/build_tool.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 source ../script/public_config.sh
+cmake_tool=${root_path}/cmake/toolchain.cmake
 
 rm -rf ./build
 
 for path in $*; do
     if [ -d ./${path} ]; then
-        cmake_command="cmake -S . -B ./build -DTARGET_PATH=$path -DCMAKE_TOOLCHAIN_FILE=../cmake/toolchain.cmake"
+        cmake_command="cmake -S . -B ./build -DTARGET_PATH=$path -DCMAKE_TOOLCHAIN_FILE=${cmake_tool}"
         if [ "${system}" = "Windows" ]; then
             ${cmake_command} -G "MinGW Makefiles"
         else
